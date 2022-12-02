8ca1bc7a8b7e04d8b10c2d699a521d14fadeb0cd
Borealin <shichuning@gmail.com>
2022-12-02 11:40:18 +0800

    [FIX] fix wrong path reference

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
+import os
 import time
 import schedule
 from deploy_freenas import deploy
 from config import parse_config, schedule_job
 if __name__ == "__main__":
-    configs = parse_config("config.json")
+    configs = parse_config(os.path.join(
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                'config'
+            ),
+            'config.py'
+        ))
     schedule_job(configs, deploy)
     while True:
         schedule.run_pending()
